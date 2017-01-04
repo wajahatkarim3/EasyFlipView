e36ca9bc5a5353abf4702bc5008f782127efcd3d
Wajahat Karim <wajahatkarim3@gmil.com>
2017-01-05 00:12:21 +0500

    Initial Empty Project and library

---
+++ EasyFlipView-Demo/app/src/main/java/com/wajahatkarim3/easyflipview/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wajahatkarim3.easyflipview.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
