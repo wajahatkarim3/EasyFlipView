2188a3d54ac4e336cf632f03604df1dd064308cf
iGio90 <giovanni.rocca.90@gmail.com>
2017-07-18 14:21:17 +0200

    Sorry travis

--- EasyFlipView-Library/src/main/java/com/wajahatkarim3/easyflipview/EasyFlipView.java
+++ EasyFlipView-Library/src/main/java/com/wajahatkarim3/easyflipview/EasyFlipView.java
@@ -9,6 +9,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 /**
